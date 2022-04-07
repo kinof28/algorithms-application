0aaffdfbc96193e461ce7e8323c6880c37624a88
Abdelwahab <ab28fb@gmail.com>
2022-04-07 04:45:54 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { GeniticAlgorithmsComponent } from './components/genitic-algorithms/genitic-algorithms.component';
+import { ShortestPathComponent } from './components/shortest-path/shortest-path.component';
+import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
+import { DocumentationComponent } from './components/documentation/documentation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    GeniticAlgorithmsComponent,
+    ShortestPathComponent,
+    NotFoundPageComponent,
+    DocumentationComponent
   ],
   imports: [
     BrowserModule,
